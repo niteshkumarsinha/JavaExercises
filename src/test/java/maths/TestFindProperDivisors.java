44a42c24ca04ac28971c86252a6febe601c76e07
niteshkumar1403 <87249271+niteshkumar1403@users.noreply.github.com>
2024-01-18 20:38:27 +0530

    test find proper divisors

--- src/test/java/maths/TestFindProperDivisors.java
+++ src/test/java/maths/TestFindProperDivisors.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -32,5 +36,20 @@ public class TestFindProperDivisors {
             vals = List.of(1);
         }
         Assertions.assertIterableEquals(findProperDivisors.findProperDivisors(val), vals);
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsStream")
+    void testFindProperDivisors(int actual, List<Integer> expected){
+        var result = findProperDivisors.findProperDivisors(actual);
+        Assertions.assertEquals(result, expected);
+    }
+
+    public static Stream<Arguments> argumentsStream(){
+        return Stream.of(
+                Arguments.of(6, List.of(1, 2, 3)),
+                Arguments.of(24, List.of(1, 2, 3, 4, 6, 8, 12)),
+                Arguments.of(7, List.of(1))
+        );
     }
 }
